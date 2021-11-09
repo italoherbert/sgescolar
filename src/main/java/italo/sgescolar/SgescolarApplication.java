87aacbce0bc247fb8d10a295a5e290b60760ceb5
fernandosje <fernandosmoraes.sje@gmail.com>
2021-11-08 22:35:12 -0300

    Melhoramento de aparência com bootstrap

---
+++ src/main/java/italo/sgescolar/SgescolarApplication.java
@@ -0,0 +1,13 @@
+package italo.sgescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgescolarApplication.class, args);
+	}
+
+}
